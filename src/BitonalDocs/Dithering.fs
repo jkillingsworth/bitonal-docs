1785279d509feacc4c77251a3681764061aaf9a0
Jim Killingsworth <jkillingsworth@gmail.com>
2014-11-28 00:54:10 -0500

    Update threshold dithering brightness calculation

--- src/BitonalDocs/Dithering.fs
+++ src/BitonalDocs/Dithering.fs
@@ -18,9 +18,9 @@ type GetPixelColor = (int * int) -> Color
 let threshold value (getPixelColor : GetPixelColor) (x, y) =
 
     let color = getPixelColor (x, y)
-    let r = int color.R
-    let g = int color.G
-    let b = int color.B
-    let brightness = (r + r + r + b + g + g + g + g) >>> 3
+    let r = int color.R * 2
+    let g = int color.G * 5
+    let b = int color.B * 1
+    let brightness = (r + g + b) / 8
 
     (byte brightness > value)
