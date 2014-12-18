b0d53dffd90b3876b840733a84de501bf754a8de
Jim Killingsworth <jkillingsworth@gmail.com>
2014-12-18 02:47:11 -0500

    Create new project for unit tests

---
+++ tests/BitonalDocs.Tests/Dithering.fs
@@ -0,0 +1,11 @@
+﻿module BitonalDocs.Tests.Dithering
+
+open NUnit.Framework
+
+//-------------------------------------------------------------------------------------------------
+
+[<Test>]
+let ``This test should pass`` () = Assert.Pass()
+
+[<Test>]
+let ``This test should fail`` () = Assert.Fail()
