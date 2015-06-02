bc5bb54e7c50af2077f063aab9aef95b610728d8
Jim Killingsworth <jkillingsworth@gmail.com>
2015-06-02 12:45:37 -0400

    Rearrange code in compression module

--- src/BitonalDocs/Compression.fs
+++ src/BitonalDocs/Compression.fs
@@ -12,71 +12,6 @@ type CompressionType =
 
 //-------------------------------------------------------------------------------------------------
 
-let compressUsingPackBits scanline =
-
-    let maxRunLength = 128
-
-    let (|BeginNextItemsToBePacked|BeginNextItemsToBeInline|End|) =
-        function
-        | head :: next1 :: next2 :: tail when head = next1 && head = next2
-            -> BeginNextItemsToBePacked (head, next1 :: next2 :: tail)
-        | head :: tail
-            -> BeginNextItemsToBeInline (head, tail)
-        | []
-            -> End
-
-    let (|PackedHasReachedRunLimit|PackedNextItemToBePacked|PackedNextItemToBeInline|PackedHasReachedInputEnd|) =
-        function
-        | (count, items), input when count = (1 - maxRunLength)
-            -> PackedHasReachedRunLimit (count, items, input)
-        | (count, items), head :: tail when head = List.head items
-            -> PackedNextItemToBePacked (count, items, tail)
-        | (count, items), head :: tail
-            -> PackedNextItemToBeInline (count, items, head :: tail)
-        | (count, items), []
-            -> PackedHasReachedInputEnd (count, items, List.empty<byte>)
-
-    let (|InlineHasReachedRunLimit|InlineNextItemToBePacked|InlineNextItemToBeInline|InlineHasReachedInputEnd|) =
-        function
-        | (count, items), input when count = (maxRunLength - 1)
-            -> InlineHasReachedRunLimit (count, items, input)
-        | (count, items), head :: next1 :: next2 :: tail when head = next1 && head = next2
-            -> InlineNextItemToBePacked (count, items, head :: next1 :: next2 :: tail)
-        | (count, items), head :: tail
-            -> InlineNextItemToBeInline (count, items, head, tail)
-        | (count, items), []
-            -> InlineHasReachedInputEnd (count, items, List.empty<byte>)
-
-    let rec loop acc = function
-        | BeginNextItemsToBePacked (head, tail) -> loopPacked acc ((0, [head]), tail)
-        | BeginNextItemsToBeInline (head, tail) -> loopInline acc ((0, [head]), tail)
-        | End -> acc
-    and loopPacked acc = function
-        | PackedHasReachedRunLimit (count, items, input)
-        | PackedHasReachedInputEnd (count, items, input)
-        | PackedNextItemToBeInline (count, items, input) -> loop ((count, items) :: acc) input
-        | PackedNextItemToBePacked (count, items, tail)
-            ->
-            let state = count - 1, items
-            loopPacked acc (state, tail)
-    and loopInline acc = function
-        | InlineHasReachedRunLimit (count, items, input)
-        | InlineHasReachedInputEnd (count, items, input)
-        | InlineNextItemToBePacked (count, items, input) -> loop ((count, items) :: acc) input
-        | InlineNextItemToBeInline (count, items, head, tail)
-            ->
-            let state = count + 1, head :: items
-            loopInline acc (state, tail)
-
-    scanline
-    |> List.ofArray
-    |> loop []
-    |> List.map (fun (count, items) -> (byte count) :: (List.rev items))
-    |> List.reduce (fun x y -> y @ x)
-    |> List.toArray
-
-//-------------------------------------------------------------------------------------------------
-
 type private CodeWordElement<'T> = { Black : 'T; White : 'T }
 
 let private createLookup values =
@@ -243,6 +178,71 @@ let compressUsingGroup3 scanline =
     |> List.ofArray
     |> loop [] (0, White)
     |> List.map computeCodeWordsForRun
+    |> List.reduce (fun x y -> y @ x)
+    |> List.toArray
+
+//-------------------------------------------------------------------------------------------------
+
+let compressUsingPackBits scanline =
+
+    let maxRunLength = 128
+
+    let (|BeginNextItemsToBePacked|BeginNextItemsToBeInline|End|) =
+        function
+        | head :: next1 :: next2 :: tail when head = next1 && head = next2
+            -> BeginNextItemsToBePacked (head, next1 :: next2 :: tail)
+        | head :: tail
+            -> BeginNextItemsToBeInline (head, tail)
+        | []
+            -> End
+
+    let (|PackedHasReachedRunLimit|PackedNextItemToBePacked|PackedNextItemToBeInline|PackedHasReachedInputEnd|) =
+        function
+        | (count, items), input when count = (1 - maxRunLength)
+            -> PackedHasReachedRunLimit (count, items, input)
+        | (count, items), head :: tail when head = List.head items
+            -> PackedNextItemToBePacked (count, items, tail)
+        | (count, items), head :: tail
+            -> PackedNextItemToBeInline (count, items, head :: tail)
+        | (count, items), []
+            -> PackedHasReachedInputEnd (count, items, List.empty<byte>)
+
+    let (|InlineHasReachedRunLimit|InlineNextItemToBePacked|InlineNextItemToBeInline|InlineHasReachedInputEnd|) =
+        function
+        | (count, items), input when count = (maxRunLength - 1)
+            -> InlineHasReachedRunLimit (count, items, input)
+        | (count, items), head :: next1 :: next2 :: tail when head = next1 && head = next2
+            -> InlineNextItemToBePacked (count, items, head :: next1 :: next2 :: tail)
+        | (count, items), head :: tail
+            -> InlineNextItemToBeInline (count, items, head, tail)
+        | (count, items), []
+            -> InlineHasReachedInputEnd (count, items, List.empty<byte>)
+
+    let rec loop acc = function
+        | BeginNextItemsToBePacked (head, tail) -> loopPacked acc ((0, [head]), tail)
+        | BeginNextItemsToBeInline (head, tail) -> loopInline acc ((0, [head]), tail)
+        | End -> acc
+    and loopPacked acc = function
+        | PackedHasReachedRunLimit (count, items, input)
+        | PackedHasReachedInputEnd (count, items, input)
+        | PackedNextItemToBeInline (count, items, input) -> loop ((count, items) :: acc) input
+        | PackedNextItemToBePacked (count, items, tail)
+            ->
+            let state = count - 1, items
+            loopPacked acc (state, tail)
+    and loopInline acc = function
+        | InlineHasReachedRunLimit (count, items, input)
+        | InlineHasReachedInputEnd (count, items, input)
+        | InlineNextItemToBePacked (count, items, input) -> loop ((count, items) :: acc) input
+        | InlineNextItemToBeInline (count, items, head, tail)
+            ->
+            let state = count + 1, head :: items
+            loopInline acc (state, tail)
+
+    scanline
+    |> List.ofArray
+    |> loop []
+    |> List.map (fun (count, items) -> (byte count) :: (List.rev items))
     |> List.reduce (fun x y -> y @ x)
     |> List.toArray
 
--- tests/BitonalDocs.Tests/Compression.fs
+++ tests/BitonalDocs.Tests/Compression.fs
@@ -7,132 +7,6 @@ open BitonalDocs.Compression
 
 //-------------------------------------------------------------------------------------------------
 
-[<Test>]
-let ``Pack bits, compression works correctly`` () =
-
-    let scanline =
-        [| 0xAAuy; 0xAAuy; 0xAAuy;
-           0x80uy; 0x00uy; 0x2Auy;
-           0xAAuy; 0xAAuy; 0xAAuy; 0xAAuy;
-           0x80uy; 0x00uy; 0x2Auy; 0x22uy;
-           0xAAuy; 0xAAuy; 0xAAuy; 0xAAuy; 0xAAuy; 0xAAuy; 0xAAuy; 0xAAuy; 0xAAuy; 0xAAuy; |]
-
-    let result = compressUsingPackBits scanline
-    let expected =
-        [| 0xFEuy; 0xAAuy;
-           0x02uy; 0x80uy; 0x00uy; 0x2Auy;
-           0xFDuy; 0xAAuy;
-           0x03uy; 0x80uy; 0x00uy; 0x2Auy; 0x22uy;
-           0xF7uy; 0xAAuy; |]
-
-    result |> should equal expected
-
-[<Test>]
-let ``Pack bits, matching sequence of 127`` () =
-
-    let scanline = Array.init 127 (fun i -> 0xAAuy)
-    let result = compressUsingPackBits scanline
-    let expected =
-        [| 0x82uy; 0xAAuy; |]
-
-    result |> should equal expected
-
-[<Test>]
-let ``Pack bits, matching sequence of 128`` () =
-
-    let scanline = Array.init 128 (fun i -> 0xAAuy)
-    let result = compressUsingPackBits scanline
-    let expected =
-        [| 0x81uy; 0xAAuy; |]
-
-    result |> should equal expected
-
-[<Test>]
-let ``Pack bits, matching sequence of 129`` () =
-
-    let scanline = Array.init 129 (fun i -> 0xAAuy)
-    let result = compressUsingPackBits scanline
-    let expected =
-        [| 0x81uy; 0xAAuy; 0x00uy; 0xAAuy; |]
-
-    result |> should equal expected
-
-[<Test>]
-let ``Pack bits, matching sequence of 2`` () =
-
-    let scanline = Array.init 2 (fun i -> 0xAAuy)
-    let result = compressUsingPackBits scanline
-    let expected =
-        [| 0x01uy; 0xAAuy; 0xAAuy; |]
-
-    result |> should equal expected
-
-[<Test>]
-let ``Pack bits, matching sequence of 3`` () =
-
-    let scanline = Array.init 3 (fun i -> 0xAAuy)
-    let result = compressUsingPackBits scanline
-    let expected =
-        [| 0xFEuy; 0xAAuy; |]
-
-    result |> should equal expected
-
-[<Test>]
-let ``Pack bits, non-matching sequence of 127`` () =
-
-    let scanline = Array.init 127 (fun i -> if (i % 2) = 0 then 0xAAuy else 0xBBuy)
-    let result = compressUsingPackBits scanline
-    let expected =
-        Array.concat
-             [ [| 0x7Euy; |]; scanline.[000 .. 126]; ]
-
-    result |> should equal expected
-
-[<Test>]
-let ``Pack bits, non-matching sequence of 128`` () =
-
-    let scanline = Array.init 128 (fun i -> if (i % 2) = 0 then 0xAAuy else 0xBBuy)
-    let result = compressUsingPackBits scanline
-    let expected =
-        Array.concat
-             [ [| 0x7Fuy; |]; scanline.[000 .. 127]; ]
-
-    result |> should equal expected
-
-[<Test>]
-let ``Pack bits, non-matching sequence of 129`` () =
-
-    let scanline = Array.init 129 (fun i -> if (i % 2) = 0 then 0xAAuy else 0xBBuy)
-    let result = compressUsingPackBits scanline
-    let expected =
-        Array.concat
-             [ [| 0x7Fuy; |]; scanline.[000 .. 127];
-               [| 0x00uy; |]; scanline.[128 .. 128]; ]
-
-    result |> should equal expected
-
-[<Test>]
-let ``Pack bits, non-matching sequence of 2`` () =
-
-    let scanline = Array.init 2 (fun i -> if (i % 2) = 0 then 0xAAuy else 0xBBuy)
-    let result = compressUsingPackBits scanline
-    let expected =
-        [| 0x01uy; 0xAAuy; 0xBBuy; |]
-
-    result |> should equal expected
-
-[<Test>]
-let ``Pack bits, non-matching sequence of 3`` () =
-
-    let scanline = Array.init 3 (fun i -> if (i % 2) = 0 then 0xAAuy else 0xBBuy)
-    let result = compressUsingPackBits scanline
-    let expected =
-        [| 0x02uy; 0xAAuy; 0xBBuy; 0xAAuy; |]
-
-    result |> should equal expected
-
-//-------------------------------------------------------------------------------------------------
-
 let private group3StringToPixels value =
     value
     |> Seq.map (fun i -> if i = '0' then Black else White)
@@ -276,3 +150,129 @@ let ``Group 3, matching white sequence of 7000`` () =
     let expected = "000000011111" + "000000011111" + "00000001100" + "0101000"
     let scanline = Array.create 7000 '1'
     group3Test scanline expected
+
+//-------------------------------------------------------------------------------------------------
+
+[<Test>]
+let ``Pack bits, compression works correctly`` () =
+
+    let scanline =
+        [| 0xAAuy; 0xAAuy; 0xAAuy;
+           0x80uy; 0x00uy; 0x2Auy;
+           0xAAuy; 0xAAuy; 0xAAuy; 0xAAuy;
+           0x80uy; 0x00uy; 0x2Auy; 0x22uy;
+           0xAAuy; 0xAAuy; 0xAAuy; 0xAAuy; 0xAAuy; 0xAAuy; 0xAAuy; 0xAAuy; 0xAAuy; 0xAAuy; |]
+
+    let result = compressUsingPackBits scanline
+    let expected =
+        [| 0xFEuy; 0xAAuy;
+           0x02uy; 0x80uy; 0x00uy; 0x2Auy;
+           0xFDuy; 0xAAuy;
+           0x03uy; 0x80uy; 0x00uy; 0x2Auy; 0x22uy;
+           0xF7uy; 0xAAuy; |]
+
+    result |> should equal expected
+
+[<Test>]
+let ``Pack bits, matching sequence of 127`` () =
+
+    let scanline = Array.init 127 (fun i -> 0xAAuy)
+    let result = compressUsingPackBits scanline
+    let expected =
+        [| 0x82uy; 0xAAuy; |]
+
+    result |> should equal expected
+
+[<Test>]
+let ``Pack bits, matching sequence of 128`` () =
+
+    let scanline = Array.init 128 (fun i -> 0xAAuy)
+    let result = compressUsingPackBits scanline
+    let expected =
+        [| 0x81uy; 0xAAuy; |]
+
+    result |> should equal expected
+
+[<Test>]
+let ``Pack bits, matching sequence of 129`` () =
+
+    let scanline = Array.init 129 (fun i -> 0xAAuy)
+    let result = compressUsingPackBits scanline
+    let expected =
+        [| 0x81uy; 0xAAuy; 0x00uy; 0xAAuy; |]
+
+    result |> should equal expected
+
+[<Test>]
+let ``Pack bits, matching sequence of 2`` () =
+
+    let scanline = Array.init 2 (fun i -> 0xAAuy)
+    let result = compressUsingPackBits scanline
+    let expected =
+        [| 0x01uy; 0xAAuy; 0xAAuy; |]
+
+    result |> should equal expected
+
+[<Test>]
+let ``Pack bits, matching sequence of 3`` () =
+
+    let scanline = Array.init 3 (fun i -> 0xAAuy)
+    let result = compressUsingPackBits scanline
+    let expected =
+        [| 0xFEuy; 0xAAuy; |]
+
+    result |> should equal expected
+
+[<Test>]
+let ``Pack bits, non-matching sequence of 127`` () =
+
+    let scanline = Array.init 127 (fun i -> if (i % 2) = 0 then 0xAAuy else 0xBBuy)
+    let result = compressUsingPackBits scanline
+    let expected =
+        Array.concat
+             [ [| 0x7Euy; |]; scanline.[000 .. 126]; ]
+
+    result |> should equal expected
+
+[<Test>]
+let ``Pack bits, non-matching sequence of 128`` () =
+
+    let scanline = Array.init 128 (fun i -> if (i % 2) = 0 then 0xAAuy else 0xBBuy)
+    let result = compressUsingPackBits scanline
+    let expected =
+        Array.concat
+             [ [| 0x7Fuy; |]; scanline.[000 .. 127]; ]
+
+    result |> should equal expected
+
+[<Test>]
+let ``Pack bits, non-matching sequence of 129`` () =
+
+    let scanline = Array.init 129 (fun i -> if (i % 2) = 0 then 0xAAuy else 0xBBuy)
+    let result = compressUsingPackBits scanline
+    let expected =
+        Array.concat
+             [ [| 0x7Fuy; |]; scanline.[000 .. 127];
+               [| 0x00uy; |]; scanline.[128 .. 128]; ]
+
+    result |> should equal expected
+
+[<Test>]
+let ``Pack bits, non-matching sequence of 2`` () =
+
+    let scanline = Array.init 2 (fun i -> if (i % 2) = 0 then 0xAAuy else 0xBBuy)
+    let result = compressUsingPackBits scanline
+    let expected =
+        [| 0x01uy; 0xAAuy; 0xBBuy; |]
+
+    result |> should equal expected
+
+[<Test>]
+let ``Pack bits, non-matching sequence of 3`` () =
+
+    let scanline = Array.init 3 (fun i -> if (i % 2) = 0 then 0xAAuy else 0xBBuy)
+    let result = compressUsingPackBits scanline
+    let expected =
+        [| 0x02uy; 0xAAuy; 0xBBuy; 0xAAuy; |]
+
+    result |> should equal expected
